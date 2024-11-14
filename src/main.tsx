700f35582384e07d95c564da3d84fddb2570dc75
stewicca <stewicalf@gmail.com>
2024-11-15 01:57:08 +0700

    [FEATURE] add login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./styles/index.css";
+import Login from "@/pages/login/login.tsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="login" element={<Login />} />
+    </>,
+  ),
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Hello World</h1>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
---
+++ src/pages/login/login.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/pages/login/features/login-form.tsx";
+
+export default function Login() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center px-4">
+      <LoginForm />
+    </div>
+  );
+}
